7bd555fed612321978129e672f427d75debe0844
Jihyun <jhjh3903@gmail.com>
2023-03-09 18:28:27 +0900

    feat : 민혁님 파일과 병합 및 page css 최대넓이 조정

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
+import Login from "./pages/login";
 import SignUp from "./pages/SignUp";
 import SellerSignUp from "./pages/SellerSignUp";
 import FindId from "./pages/FindId";
@@ -11,8 +11,10 @@ import Footer from "./components/footer/Footer";
 import styled from "styled-components";
 import Pay from "./pages/Pay";
 import PayComplete from "./pages/PayComplete";
+import Mypage from "./pages/Mypage";
+import SellerMypage from "./pages/SellerMypage";
+import AdminMypage from "./pages/AdminMypage";
 
-const Content = styled.div``;
 function App() {
   return (
     <BrowserRouter>
@@ -27,6 +29,9 @@ function App() {
           <Route path="/findPassWord" element={<FindPassWord />} />
           <Route path="/pay" element={<Pay />} />
           <Route path="/payComplete" element={<PayComplete />} />
+          <Route path="/mypage" element={<Mypage />}></Route>
+          <Route path="/sellermypage" element={<SellerMypage />}></Route>
+          <Route path="/adminmypage" element={<AdminMypage />}></Route>
         </Routes>
       </Wrapper>
 
@@ -38,7 +43,8 @@ function App() {
 export default App;
 
 const Wrapper = styled.div`
+  max-width: 1450px;
   min-height: 100%;
   padding-bottom: 200px;
-  margin: 0 6rem;
+  margin: 0 auto;
 `;
