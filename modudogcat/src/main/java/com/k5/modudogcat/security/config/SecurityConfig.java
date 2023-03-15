0b077c97bd65e5a71d4d59cf93122b4d337f1b43
steadykyu <awakekyu1@gmail.com>
2023-03-16 04:02:48 +0900

    feat : 기본 로그인 기능 구현

---
+++ modudogcat/src/main/java/com/k5/modudogcat/security/config/SecurityConfig.java
@@ -0,0 +1,87 @@
+package com.k5.modudogcat.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity(debug = true)
+public class SecurityConfig {
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer(){
+        return (web) -> web.ignoring()
+                .antMatchers(HttpMethod.POST, "/users");
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+
+        http
+                .authorizeRequests(authorize -> authorize
+                        //Todo: 인증, 인가가 필요한 요청들 넣어두기
+                        .antMatchers(HttpMethod.GET,"/users/**").hasRole("USER")
+                        .anyRequest().permitAll()
+                )
+                .httpBasic()
+                    .disable()
+                .formLogin()
+                    .permitAll()
+                .and()
+                .cors(httpSecurityCorsConfigurer -> withDefaults())
+                .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
+                .csrf()
+                    .disable()
+                //CORS 요청시, 수정 필요할 수도 있음
+                .headers().frameOptions().sameOrigin()
+
+                .and()
+                .logout()
+                .logoutUrl("/logout");
+//                .logoutSuccessHandler(new CustomLogoutHandler());
+//        http
+//                .apply(new CustomFilterConfigurer());
+//        http
+//                .oauth2Login().successHandler(new OAuth2SuccessHandler(userService));
+//        http
+
+        return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOriginPatterns(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+//        configuration.setExposedHeaders(Arrays.asList("Authorization", "Location", "Refresh"));
+//        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+}
