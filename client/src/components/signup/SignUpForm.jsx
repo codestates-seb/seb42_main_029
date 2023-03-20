b23a217e392a24f1e56ce4933a21eaeaf47599a4
Min Hyeok <96058996+Min-hh-k@users.noreply.github.com>
2023-03-20 10:50:50 +0900

    feat : dev/fe npm i

--- client/src/components/signup/SignUpForm.jsx
+++ client/src/components/signup/SignUpForm.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-// import axios from "axios";
+import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { useCookies } from "react-cookie";
 
