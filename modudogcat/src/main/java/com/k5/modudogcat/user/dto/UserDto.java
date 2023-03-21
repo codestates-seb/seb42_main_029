2c2aa6e3a37f3b0b6080218e5085f9f2bcfc663e
steadykyu <awakekyu1@gmail.com>
2023-03-21 09:49:24 +0900

    feat : userDto 유효성 검사 추가

--- modudogcat/src/main/java/com/k5/modudogcat/user/dto/UserDto.java
+++ modudogcat/src/main/java/com/k5/modudogcat/user/dto/UserDto.java
@@ -6,6 +6,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 public class UserDto {
@@ -13,9 +17,15 @@ public class UserDto {
     @NoArgsConstructor
     @AllArgsConstructor
     public static class Post {
+        @NotBlank(message = "로그인ID는 필수 입력 값입니다.")
         private String loginId;
+        @NotBlank(message = "이름은 필수 입력 값입니다.")
         private String name;
+        @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
+        @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
         private String password;
+        @Pattern(regexp = "^(?:\\w+\\.?)*\\w+@(?:\\w+\\.)+\\w+$", message = "이메일 형식이 올바르지 않습니다.")
+        @NotBlank(message = "이메일은 필수 입력 값입니다.")
         private String email;
         private String address;
 
@@ -27,6 +37,7 @@ public class UserDto {
     @AllArgsConstructor
     public static class Patch{
         private Long userId;
+        @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
         private String password;
         private String address;
     }
