3c25d0da82ab5c839ef7f045824daf7d0ca5b084
RYE415 <rhk415@gmail.com>
2023-03-23 15:55:56 +0900

    "chore : Modify directory constructure"

---
+++ modudogcat/.github/ISSUE_TEMPLATE/feature-issue-template.md
@@ -0,0 +1,35 @@
+---
+name: Feature issue template
+about: Feature issue template
+title: ''
+labels: ''
+assignees: ''
+
+---
+
+## 만들고자 하는 기능
+ex) 로그인 기능
+
+<br>  
+
+## 기능의 설명
+ex) 웹 사이트 로그인 진행  
+
+<br> 
+
+## Todo  
+1. [ ] Job1
+2. [ ] Job2
+3. [ ] Job3
+
+<br> 
+
+## Issue
+ex) 요청을 어떻게 할지, 데이터 모델링을 어떻게 구성할지
+
+<br> 
+
+## 기타사항
+ex) ...
+
+<br>
---
+++ src/main/java/com/k5/modudogcat/domain/user/controller/UserController.java
@@ -0,0 +1,78 @@
+package com.k5.modudogcat.domain.user.controller;
+
+import com.k5.modudogcat.domain.user.dto.UserDto;
+import com.k5.modudogcat.domain.user.entity.User;
+import com.k5.modudogcat.domain.user.service.UserService;
+import com.k5.modudogcat.dto.MultiResponseDto;
+import com.k5.modudogcat.dto.SingleResponseDto;
+import com.k5.modudogcat.domain.user.Mapper.UserMapper;
+
+import com.k5.modudogcat.util.UriCreator;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+@Validated
+public class UserController {
+    private final static String USER_DEFAULT_URL = "/users/";
+    private final UserMapper mapper;
+    private final UserService userService;
+
+    @PostMapping("/sign-up")
+    public ResponseEntity postUser(@RequestBody UserDto.Post postDto){
+        User user = mapper.userPostToUser(postDto);
+        User findUser = userService.createUser(user);
+
+        Long userId = findUser.getUserId();
+        URI location = UriCreator.createUri(USER_DEFAULT_URL, userId);
+
+        return ResponseEntity.created(location).build();
+    }
+
+    @PatchMapping("/{user-id}")
+    public ResponseEntity patchUser(@PathVariable("user-id") Long userId,
+                                    @RequestBody UserDto.Patch patchDto){
+        patchDto.setUserId(userId);
+        User user = mapper.userPatchToUser(patchDto);
+        User updateUser = userService.updateUser(user);
+        UserDto.Response response = mapper.userToUserResponse(updateUser);
+
+        return new ResponseEntity(new SingleResponseDto<>(response), HttpStatus.OK);
+    }
+
+    @GetMapping("/my-page")
+    public ResponseEntity getUser(){
+        String principal = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        long userId = Long.parseLong(principal);
+        User findUser = userService.findVerifiedUserById(userId);
+        UserDto.Response response = mapper.userToUserResponse(findUser);
+
+        return new ResponseEntity(new SingleResponseDto<>(response), HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity getUsers(Pageable pageable){
+        Page<User> pageUsers = userService.findUsers(pageable);
+        List<User> users = pageUsers.getContent();
+        List<UserDto.Response> responses = mapper.usersToUsersResponse(users);
+
+        return new ResponseEntity(new MultiResponseDto<>(
+               responses, pageUsers), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{user-id}")
+    public ResponseEntity deleteUser(@PathVariable("user-id") long userId){
+        userService.removeUser(userId);
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+}
