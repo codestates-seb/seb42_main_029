5e69272a8aab73114bd0e399818477662923781e
RYE415 <rhk415@gmail.com>
2023-03-23 12:45:18 +0900

    "chore : deploy 파일 변경"

--- modudogcat/src/test/java/com/k5/modudogcat/ModudogcatApplicationTests.java
+++ modudogcat/src/test/java/com/k5/modudogcat/ModudogcatApplicationTests.java
@@ -3,7 +3,7 @@ package com.k5.modudogcat;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class ModudogcatApplicationTests {
 
 	@Test
