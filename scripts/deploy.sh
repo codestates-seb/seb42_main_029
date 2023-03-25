d81f77486a965406b85d38032a987d34a856cdc2
RYE415 <rhk415@gmail.com>
2023-03-25 19:18:36 +0900

    fix : Add time at deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -35,4 +35,8 @@ echo "> 현재 디렉토리: $(pwd)" >> /home/ubuntu/deploy-b/deploy.log
 echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/deploy-b/deploy.log
 nohup java -jar -Dspring.profiles.active=server $JAR_NAME >> /home/ubuntu/deploy-b/resultDeploy.log 2>/home/ubuntu/deploy-b/deploy_err.log &
 
-echo "> ---------------------------------------------------" >> /home/ubuntu/deploy-b/deploy.log
+echo "> --------------------------------------------------------------------------------------" >> /home/ubuntu/deploy-b/resultDeploy.log 2>/home/ubuntu/deploy-b/deploy_err.log &
+echo "> 현재 시간: $(date)"
+echo "> --------------------------------------------------------------------------------------" >> /home/ubuntu/deploy-b/resultDeploy.log 2>/home/ubuntu/deploy-b/deploy_err.log &
+
+echo "> --------------------------------------------------------------------------------------" >> /home/ubuntu/deploy-b/deploy.log
