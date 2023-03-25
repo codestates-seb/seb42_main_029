f17bb138e4835600e9984d33902dc104455656d6
RYE415 <rhk415@gmail.com>
2023-03-25 18:29:56 +0900

    fix : Modify deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -31,12 +31,12 @@ echo "> $JAR_NAME 실행 권한 추가" >> /home/ubuntu/deploy-b/deploy.log
 DEPLOYBUILD_PATH=/home/ubuntu/deploy-b/build/libs/
 
 # shellcheck disable=SC2164
-cd $DEPLOY_PATH
+cd $DEPLOYBUILD_PATH
 
 echo "> 현재 디렉토리: $(pwd)" >> /home/ubuntu/deploy-b/deploy.log
 
 echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/deploy-b/deploy.log
 
-nohup java -jar -Dspring.profiles.active=server $DEPLOYBUILD_PATH$JAR_NAME >/home/ubuntu/deploy-b/result-deploy.log &
+nohup java -jar -Dspring.profiles.active=server $JAR_NAME >/home/ubuntu/deploy-b/result-deploy.log &
 
 echo "> ---------------------------------------------------" >> /home/ubuntu/deploy-b/deploy.log
