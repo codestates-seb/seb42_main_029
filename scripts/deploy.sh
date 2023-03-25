7cab48fafc30c5c0896c5df2f733eff3aa805593
RYE415 <rhk415@gmail.com>
2023-03-25 18:52:37 +0900

    fix : Modify deploy.sh add sudo

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -36,6 +36,6 @@ chmod +x $JAR_NAME
 echo "> 현재 디렉토리: $(pwd)" >> /home/ubuntu/deploy-b/deploy.log
 
 echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/deploy-b/deploy.log
-nohup java -jar -Dspring.profiles.active=server $JAR_NAME >> /home/ubuntu/deploy-b/resultDeploy.log 2>/home/ubuntu/deploy-b/deploy_err.log &
+sudo nohup java -jar -Dspring.profiles.active=server $JAR_NAME >> /home/ubuntu/deploy-b/resultDeploy.log 2>/home/ubuntu/deploy-b/deploy_err.log &
 
 echo "> ---------------------------------------------------" >> /home/ubuntu/deploy-b/deploy.log
