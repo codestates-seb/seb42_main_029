f4907d6e43460875ccb7adc1a1980fe0469edac2
RYE415 <rhk415@gmail.com>
2023-03-26 01:29:33 +0900

    fix : jwt.key test

--- src/main/java/com/k5/modudogcat/ModudogcatApplication.java
+++ src/main/java/com/k5/modudogcat/ModudogcatApplication.java
@@ -1,14 +1,19 @@
 package com.k5.modudogcat;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @EnableJpaAuditing
 @SpringBootApplication
 public class ModudogcatApplication {
 
+	@Value("${jwt.key}")
+	private static String jwt;
 	public static void main(String[] args) {
+		System.out.println(jwt);
 		SpringApplication.run(ModudogcatApplication.class, args);
 	}
 
