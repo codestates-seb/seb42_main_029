b3e82cb234cd4309a9913c041d32b609409087c2
RYE415 <rhk415@gmail.com>
2023-03-25 18:48:19 +0900

    fix : Modify deploy.sh 실행 권한, 로그 디렉토리

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -28,16 +28,14 @@ fi
 
 DEPLOYBUILD_PATH=/home/ubuntu/deploy-b/build/libs/
 
-cd $DEPLOYBUILD_PATH
+cd $DEPLOY_PATH
 
 echo "> $JAR_NAME 실행 권한 추가" >> /home/ubuntu/deploy-b/deploy.log
-
-chmod 755 $JAR_NAME
+chmod +x $JAR_NAME
 
 echo "> 현재 디렉토리: $(pwd)" >> /home/ubuntu/deploy-b/deploy.log
 
 echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/deploy-b/deploy.log
-
-nohup java -jar -Dspring.profiles.active=server $JAR_NAME >/home/ubuntu/deploy-b/result-deploy.log &
+nohup java -jar -Dspring.profiles.active=server $JAR_NAME >> /home/ubuntu/deploy-b/resultDeploy.log 2>/home/ubuntu/deploy-b/deploy_err.log &
 
 echo "> ---------------------------------------------------" >> /home/ubuntu/deploy-b/deploy.log
