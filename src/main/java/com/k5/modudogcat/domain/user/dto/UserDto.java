591353b734e88ce73c726fa7dd7612fd995266f8
RYE415 <rhk415@gmail.com>
2023-03-27 18:10:14 +0900

    fix : 유효성 검증 수정

--- src/main/java/com/k5/modudogcat/domain/user/dto/UserDto.java
+++ src/main/java/com/k5/modudogcat/domain/user/dto/UserDto.java
@@ -35,7 +35,7 @@ public class UserDto {
     @AllArgsConstructor
     public static class Patch{
         private Long userId;
-        @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
+        @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[\\W_]).{8,}$", message = "비밀번호는 8자 이상의 알파벳과 숫자와 특수문자로 이루어져야 합니다.")
         private String password;
         @Pattern(regexp = "^(?:\\w+\\.?)*\\w+@(?:\\w+\\.)+\\w+$", message = "이메일 형식이 올바르지 않습니다.")
         private String email;
